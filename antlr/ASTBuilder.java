922af909cd85c4341eff82e1fc2414c4da4a743b
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2025-07-05 12:19:41 -0300

    Implementa os visit DEF, DATAREGULAR, DATABSTRACT, FUN, PARAM E PARAMS

--- antlr/ASTBuilder.java
+++ antlr/ASTBuilder.java
@@ -32,5 +32,73 @@ public class ASTBuilder extends langBaseVisitor<Object> {
         return new Type(baseType, arrayDepth);
     }
 
+    @Override
+    public Def visitDef(langParser.DefContext ctx) {
+        if (ctx.data() != null)
+            return (Def) visit(ctx.data());
+        else
+            return (Def) visit(ctx.fun());
+    }
+
+    @Override
+    public DataRegular visitDataRegular(langParser.DataRegularContext ctx) {
+        String name = ctx.TYID().getText();
+        List<Decl> declarations = new ArrayList<>();
+        for (var declCtx : ctx.decl()){
+            declarations.add((Decl) visit(declCtx));
+        }
+        return new DataRegular(name, declarations);
+    }
+
+    @Override
+    public DataAbstract visitDataAbstract(langParser.DataAbstractContext ctx){
+        String name = ctx.TYID().getText();
+        List<Decl> declarations = new ArrayList<>();
+        List<Fun> functions = new ArrayList<>();
+
+        for (var child : ctx.children){
+            if (child instanceof langParser.DeclContext)
+                declarations.add((Decl) visit(child));
+            else if (child instanceof langParser.FunContext)
+                functions.add((Fun) visit(child));
+        }
+
+        return new DataAbstract(name, declarations, functions);
+    }
+
+    @Override
+    public Fun visitFun(langParser.FunContext ctx){
+        String name = ctx.ID().getText();
+
+        List<Param> params = new ArrayList<>();
+        if(ctx.params() != null)
+            params = (List<Param>) visit(ctx.params());
+
+        List<Type> returnTypes = new ArrayList<>();
+        if (ctx.retTypes() != null)
+            returnTypes = (List<Type>) visit(ctx.retTypes());
+
+        // Aqui poderá visitar o cmd quando estiver implementado
+        // Cmd body = (Cmd) visit(ctx.cmd());
+
+        return new Fun(name, params, returnTypes, null); //use null emporariamente para o corpo.
+    }
+
+    @Override
+    public Param visitParam(langParser.ParamContext ctx) {
+        String id = ctx.ID().getText();
+        Type type = (Type) visit(ctx.type());
+        return new Param(id, type);
+    }
+
+    @Override
+    public List<Param> visitParams(langParser.ParamsContext ctx) {
+        List<Param> list = new ArrayList<>();
+        for (var p : ctx.param()) {
+            list.add((Param) visit(p));
+        }
+        return list;
+    }
+
     // Você pode adicionar os próximos visitadores: visitFun, visitParam, visitExp, etc.
 }
