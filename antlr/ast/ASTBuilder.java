4f2a176841049ac22861711b409e56f4e5d97886
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2025-07-05 23:19:22 -0300

    Insere os visits das novas classes criadas

--- antlr/ast/ASTBuilder.java
+++ antlr/ast/ASTBuilder.java
@@ -87,10 +87,9 @@ public class ASTBuilder extends langBaseVisitor<Object> {
         if (ctx.retTypes() != null)
             returnTypes = (List<Type>) visit(ctx.retTypes());
 
-        // Aqui poderá visitar o cmd quando estiver implementado
-        // Cmd body = (Cmd) visit(ctx.cmd());
+        Cmd body = (Cmd) visit(ctx.cmd());
 
-        return new Fun(name, params, returnTypes, null); //use null emporariamente para o corpo.
+        return new Fun(name, params, returnTypes, body);
     }
 
     @Override
@@ -109,5 +108,234 @@ public class ASTBuilder extends langBaseVisitor<Object> {
         return list;
     }
 
-    // Você pode adicionar os próximos visitadores: visitFun, visitParam, visitExp, etc.
+    @Override
+    public Cmd visitCmd(langParser.CmdContext ctx) {
+        if (ctx.block() != null) return (Cmd) visit(ctx.block());
+        if (ctx.IF() != null && ctx.ELSE() != null) return new CmdIf((Exp) visit(ctx.exp(0)), (Cmd) visit(ctx.cmd(0)), (Cmd) visit(ctx.cmd(1)));
+        if (ctx.IF() != null) {
+            Exp cond = (Exp) visit(ctx.exp(0));
+            Cmd thenBranch = (Cmd) visit(ctx.cmd(0));
+            Cmd elseBranch = ctx.ELSE() != null ? (Cmd) visit(ctx.cmd(1)) : null;
+            return new CmdIf(cond, thenBranch, elseBranch);
+        }
+        if (ctx.ITERATE() != null) return new CmdIterate((ItCond) visit(ctx.itcond()), (Cmd) visit(ctx.cmd(0)));
+        if (ctx.READ() != null) return new CmdRead((LValue) visit(ctx.lvalue(0)));
+        if (ctx.PRINT() != null) return new CmdPrint((Exp) visit(ctx.exp(0)));
+        if (ctx.RETURN() != null) {
+            List<Exp> exps = new ArrayList<>();
+            for (var e : ctx.exp()) {
+                exps.add((Exp) visit(e));
+            }
+            return new CmdReturn(exps);
+        }
+        if (ctx.ASSIGN() != null) return new CmdAssign((LValue) visit(ctx.lvalue(0)), (Exp) visit(ctx.exp(0)));
+        if (ctx.ID() != null && ctx.LANGLE() == null) {
+            String id = ctx.ID().getText();
+            List<Exp> args = ctx.exps() != null ? (List<Exp>) visit(ctx.exps()) : new ArrayList<>();
+            List<LValue> rets = new ArrayList<>();  // sem valores de retorno nesse caso
+            return new CmdCall(id, args, rets);
+        }
+        if (ctx.ID() != null && ctx.LANGLE() != null) {
+            String id = ctx.ID().getText();
+            List<Exp> args = ctx.exps() != null ? (List<Exp>) visit(ctx.exps()) : new ArrayList<>();
+            List<LValue> rets = new ArrayList<>();
+            for (int i = 0; i < ctx.lvalue().size(); i++) {
+                rets.add((LValue) visit(ctx.lvalue(i)));
+            }
+            return new CmdCall(id, args, rets);
+        }
+
+        return null;
+    }
+
+    @Override
+    public Cmd visitBlock(langParser.BlockContext ctx) {
+        List<Cmd> cmds = new ArrayList<>();
+        for (var c : ctx.cmd()) {
+            cmds.add((Cmd) visit(c));
+        }
+        return new CmdBlock(cmds);
+    }
+
+     @Override
+    public ItCond visitCondLabelled(langParser.CondLabelledContext ctx) {
+        return new ItCondLabelled(ctx.ID().getText(), (Exp) visit(ctx.exp()));
+    }
+
+    @Override
+    public ItCond visitCondExpr(langParser.CondExprContext ctx) {
+        return new ItCondExpr((Exp) visit(ctx.exp()));
+    }
+
+    @Override
+    public LValue visitIdLval(langParser.IdLvalContext ctx) {
+        return new LValueId(ctx.ID().getText());
+    }
+
+    @Override
+    public LValue visitIndexLval(langParser.IndexLvalContext ctx) {
+        return new LValueIndex((LValue) visit(ctx.lvalue()), (Exp) visit(ctx.exp()));
+    }
+
+    @Override
+    public LValue visitFieldLval(langParser.FieldLvalContext ctx) {
+        return new LValueField((LValue) visit(ctx.lvalue()), ctx.ID().getText());
+    }
+
+    @Override
+    public List<Exp> visitExpsList(langParser.ExpsListContext ctx) {
+        List<Exp> exps = new ArrayList<>();
+        for (var e : ctx.exp()) {
+            exps.add((Exp) visit(e));
+        }
+        return exps;
+    }
+
+    @Override
+    public Exp visitIntExpr(langParser.IntExprContext ctx) {
+        return new ExpInt(Integer.parseInt(ctx.INT().getText()));
+    }
+
+    @Override
+    public Exp visitFloatExpr(langParser.FloatExprContext ctx) {
+        return new ExpFloat(Float.parseFloat(ctx.FLOAT().getText()));
+    }
+
+    @Override
+    public Exp visitCharExpr(langParser.CharExprContext ctx) {
+        return new ExpChar(ctx.CHAR().getText().charAt(1));
+    }
+
+    @Override
+    public Exp visitTrueExpr(langParser.TrueExprContext ctx) {
+        return new ExpBool(true);
+    }
+
+    @Override
+    public Exp visitFalseExpr(langParser.FalseExprContext ctx) {
+        return new ExpBool(false);
+    }
+
+    @Override
+    public Exp visitNullExpr(langParser.NullExprContext ctx) {
+        return new ExpNull();
+    }
+
+    @Override
+    public Exp visitLvalExpr(langParser.LvalExprContext ctx) {
+        LValue lv = (LValue) visit(ctx.lvalue());
+
+        if (lv instanceof LValueId) {
+            String varName = ((LValueId) lv).id;
+            return new ExpVar(varName);
+        } else {
+            // Caso não seja um identificador simples, não faz sentido criar ExpVar
+            throw new RuntimeException("Expected simple variable identifier (LValueId), got: " + lv.getClass().getSimpleName());
+        }
+    }
+
+    @Override
+    public Exp visitParenExpr(langParser.ParenExprContext ctx) {
+        return new ExpParen((Exp) visit(ctx.exp()));
+    }
+
+    @Override
+    public Exp visitNewExpr(langParser.NewExprContext ctx) {
+        Type t = (Type) visit(ctx.type());
+        Exp size = ctx.exp() != null ? (Exp) visit(ctx.exp()) : null;
+        return new ExpNew(t, size);
+    }
+
+    @Override
+    public Exp visitCallIndexedExpr(langParser.CallIndexedExprContext ctx) {
+        String id = ctx.ID().getText();
+        List<Exp> args = ctx.exps() != null ? (List<Exp>) visit(ctx.exps()) : new ArrayList<>();
+        Exp index = (Exp) visit(ctx.exp());
+        
+        ExpCall call = new ExpCall(id, args);
+        return new ExpCallIndexed(call, index);
+    }
+    
+    @Override
+    public Exp visitNotExpr(langParser.NotExprContext ctx) {
+        return new ExpUnaryOp("!", (Exp) visit(ctx.expMul()));
+    }
+
+    @Override
+    public Exp visitNegExpr(langParser.NegExprContext ctx) {
+        return new ExpUnaryOp("-", (Exp) visit(ctx.expMul()));
+    }
+
+    @Override
+    public Exp visitToPrimary(langParser.ToPrimaryContext ctx) {
+        return (Exp) visit(ctx.expPrimary());
+    }
+
+    @Override
+    public Exp visitAddExpr(langParser.AddExprContext ctx) {
+        Exp left = (Exp) visit(ctx.expMul(0));
+        for (int i = 1; i < ctx.expMul().size(); i++) {
+            String op = ctx.getChild(2 * i - 1).getText();
+            Exp right = (Exp) visit(ctx.expMul(i));
+            left = new ExpBinOp(op, left, right);
+        }
+        return left;
+    }
+
+    @Override
+    public Exp visitEqExpr(langParser.EqExprContext ctx) {
+        Exp left = (Exp) visit(ctx.expAdd(0));
+
+        for (int i = 1; i < ctx.expAdd().size(); i++) {
+            String op = ctx.getChild(2 * i - 1).getText();
+            Exp right = (Exp) visit(ctx.expAdd(i));
+            left = new ExpBinOp(op, left, right);
+        }
+
+        return left;
+    }
+
+    @Override
+    public Exp visitRelExpr(langParser.RelExprContext ctx) {
+        Exp left = (Exp) visit(ctx.expEq(0));
+
+        for (int i = 1; i < ctx.expEq().size(); i++) {
+            String op = ctx.getChild(2 * i - 1).getText();
+            Exp right = (Exp) visit(ctx.expEq(i));
+            left = new ExpBinOp(op, left, right);
+        }
+
+        return left;
+    }
+
+    @Override
+    public Exp visitAndExpr(langParser.AndExprContext ctx) {
+        Exp left = (Exp) visit(ctx.expRel(0));
+
+        for (int i = 1; i < ctx.expRel().size(); i++) {
+            String op = ctx.getChild(2 * i - 1).getText();
+            Exp right = (Exp) visit(ctx.expRel(i));
+            left = new ExpBinOp(op, left, right);
+        }
+
+        return left;
+    }
+
+    @Override
+    public Exp visitOrExpr(langParser.OrExprContext ctx) {
+        Exp left = (Exp) visit(ctx.expAnd(0));
+
+        for (int i = 1; i < ctx.expAnd().size(); i++) {
+            String op = ctx.getChild(2 * i - 1).getText();
+            Exp right = (Exp) visit(ctx.expAnd(i));
+            left = new ExpBinOp(op, left, right);
+        }
+
+        return left;
+    }
+
+    @Override
+    public Exp visitExpTop(langParser.ExpTopContext ctx) {
+        return (Exp) visit(ctx.expOr());
+    }
 }
